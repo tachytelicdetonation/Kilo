bf80717e8ae456e1e5b5431766268e80b6add7f7
tachytelicdetonation <deshmukhtanmay017@gmail.com>
2020-06-01 23:43:07 +0530

    First Commit

---
+++ kilo.c
@@ -0,0 +1,40 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> 
+#include <termios.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+
+    tcgetattr(STDERR_FILENO, &raw);
+
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDERR_FILENO, TCSAFLUSH, &raw);
+    
+}
+
+int main () {
+    enableRawMode();
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
+        if(iscntrl(c)) {
+            printf("%d\n", c);
+        }
+        else{
+            printf("%d ('%c)\n",c,c);
+        }
+    }
+    return 0;
+}
